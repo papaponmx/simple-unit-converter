81832758096942eac4710747a2eb5249c499b26d
Jaime Rios <jaimemadrigalrios@gmail.com>
2019-04-27 00:50:31 -0500

    Connected initial components

---
+++ src/converter-app/index.js
@@ -0,0 +1,16 @@
+/**
+ * In this file is the main 
+ * app component
+ */
+
+ import React from 'react'
+import Header from '../header';
+ 
+ export default function ConverterApp() {
+   return (
+     <div>
+       <Header />
+     </div>
+   )
+ }
+ 
---
+++ src/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/** This is the app header */
+
+export default function Header() {
+  return (
+    <header>
+      <span>TODO: Add hamburguer menu</span>
+      <h1>Simple Unit Converter</h1>
+      <span>TODO: Add Icon</span>
+    </header>
+  )
+}
