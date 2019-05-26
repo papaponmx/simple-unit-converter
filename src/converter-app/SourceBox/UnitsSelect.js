a7302d4227ab612dd3beb6851d9029edc2648644
Jaime Rios <jaimemadrigalrios@gmail.com>
2019-05-26 15:03:46 -0500

    Refactored Unit Select so it becomes reusable.

--- src/converter-app/SourceBox/UnitsSelect.js
+++ src/converter-app/SourceBox/UnitsSelect.js
@@ -1,15 +1,32 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Context as MagnitudesContext } from '../../context';
 import Select, { Option } from '../../Forms/select';
 
-const UnitsSelect = () => {
-  const { selectedMagnitude, setInputUnit } = useContext(MagnitudesContext);
+const UnitsSelect = ({ isOutput = false }) => {
+  const {
+    inputUnit,
+    selectedMagnitude,
+    setInputUnit,
+  } = useContext(MagnitudesContext);
   const { units } = selectedMagnitude;
 
   const handleSelect = (event) => {
     // IDEA: Move this function into a getters file
     const selectedInputUnit = units.find(unit => unit.name === event.target.value);
     return setInputUnit(selectedInputUnit);
+  };
+
+  const renderOptions = () => {
+    const options = isOutput
+      ? units.filter(unit => unit.name !== inputUnit.name)
+      : units;
+
+    return options.map(({ name }) => (
+      <Option key={name} value={name}>
+        {name}
+      </Option>
+    ));
   };
 
   return (
@@ -19,15 +36,17 @@ const UnitsSelect = () => {
       name="inputUnit"
       onChange={handleSelect}
     >
-      {
-      units.map(({ name }) => (
-        <Option key={name} value={name}>
-          {name}
-        </Option>
-      ))
-      }
+      {renderOptions()}
     </Select>
   );
+};
+
+UnitsSelect.defaultProps = {
+  isOutput: false,
+};
+
+UnitsSelect.propTypes = {
+  isOutput: PropTypes.bool,
 };
 
 export default UnitsSelect;
